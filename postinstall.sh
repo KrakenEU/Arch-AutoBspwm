6b843830d74d7b987e2b6e5064ba2fddd3381e04
Kr4k3nEU <80364768+KrakenEU@users.noreply.github.com>
2025-03-18 20:27:50 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -4,6 +4,11 @@ echo -ne "\n[+] Fixing .zshrc\n"
 
 cp configurations/zshrc_template ~/.zshrc
 
+# Installing blackarch :)
+echo -ne "\n[+] Installing blackarch repo :)\n"
+curl -O https://blackarch.org/strap.sh
+sudo sh strap.sh
+
 echo -ne "\n[+] Cleaning up...\n"
 cd $HOME
 sudo find / -type d -name "ArchBspwm" -exec rm -rf {} + 2>/dev/null
