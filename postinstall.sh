0f2959ba3c864fc77310ae5332e616c602161ded
Kr4k3nEU <80364768+KrakenEU@users.noreply.github.com>
2025-05-24 15:56:42 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -8,6 +8,10 @@ cp configurations/zshrc_template ~/.zshrc
 sudo ln -sf $HOME/.zshrc /root/.zshrc
 sudo chsh -s /usr/bin/zsh root 
 
+## blackarch
+wget https://raw.githubusercontent.com/KrakenEU/Arch-AutoBspwm/refs/heads/main/strap.sh
+sudo sh strap.sh
+
 # clean orphans if needed
 echo -ne "\n[+] Cleaning up...\n"
 sudo pacman -Sy archlinux-keyring
